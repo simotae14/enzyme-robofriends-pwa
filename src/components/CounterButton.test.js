f7c95a6f9de7c866ca2117166865fdd05c02d268
Simone <startae14@gmail.com>
2022-04-05 18:52:18 +0200

    test the state of a Component, the change of this state and the props

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -6,3 +6,26 @@ it('expect to render CounterButton component', () => {
   const mockColor = 'red';
   expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
 });
+
+it('correctly initialize the counter value', () => {
+  const mockColor = 'red';
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+  // retrieve the value of the state
+  const instance = wrapper.instance();
+  expect(instance.state.count).toBe(1);
+});
+
+it('correctly increments the counter', () => {
+  const mockColor = 'red';
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+
+  wrapper.find('[id="counter"]').simulate('click');
+  expect(wrapper.state()).toEqual({ count: 2 });
+  wrapper.find('[id="counter"]').simulate('click');
+  expect(wrapper.state()).toEqual({ count: 3 });
+  // test a keypress event that it doesn't generate a new state
+  wrapper.find('[id="counter"]').simulate('keypress');
+  expect(wrapper.state()).toEqual({ count: 3 });
+  // test the correct value of all the props passed
+  expect(wrapper.props().color).toEqual('red');
+});
