9b992787d63cede8f3a8fdcfc326b742e67b8e32
Simone <startae14@gmail.com>
2022-04-06 19:06:59 +0200

    test all the other reducers

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -2,7 +2,7 @@ import {
   CHANGE_SEARCHFIELD,
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
-  REQUEST_ROBOTS_FAILED
+  REQUEST_ROBOTS_FAILED,
 } from './types';
 
 import * as reducers from './reducers';
@@ -20,7 +20,7 @@ describe('searchRobots', () => {
     expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: '' });
   });
 
-  it('should handle CHANGE_SEARCHFIELD', () => {
+  it('should handle CHANGE_SEARCHFIELD action', () => {
     expect(reducers.searchRobots(
       initialStateSearch,
       {
@@ -29,6 +29,65 @@ describe('searchRobots', () => {
       }
     )).toEqual({
       searchField: 'abc',
+    });
+  });
+});
+
+
+describe('requestRobots', () => {
+  // define the initial state
+  const initialStateSearch = {
+    robots: [],
+    isPending: false,
+  };
+  it('should return the initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual({
+      robots: [],
+      isPending: false,
+    });
+  });
+  it('should handle REQUEST_ROBOTS_PENDING action', () => {
+    expect(reducers.requestRobots(
+      initialStateSearch,
+      {
+        type: REQUEST_ROBOTS_PENDING,
+      }
+    )).toEqual({
+      robots: [],
+      isPending: true,
+    });
+  });
+  it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+    expect(reducers.requestRobots(
+      initialStateSearch,
+      {
+        type: REQUEST_ROBOTS_SUCCESS,
+        payload: [{
+          id: '123',
+          name: 'test',
+          email: 'test@gmail.com',
+        }],
+      }
+    )).toEqual({
+      robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com',
+      }],
+      isPending: false,
+    });
+  });
+  it('should handle REQUEST_ROBOTS_FAILED action', () => {
+    expect(reducers.requestRobots(
+      initialStateSearch,
+      {
+        type: REQUEST_ROBOTS_FAILED,
+        payload: 'NOOOOOOOO!!!!!!!',
+      }
+    )).toEqual({
+      error: 'NOOOOOOOO!!!!!!!',
+      robots: [],
+      isPending: false,
     });
   });
 });
